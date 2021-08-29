7a22f31aa0e01fff6ddb1c8f8222fc5ca409b047
nakaji <wandora.job@gmail.com>
2021-08-29 10:30:42 +0900

    d

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "CI/CD Test3"
+    return "CI/CD Test4"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
